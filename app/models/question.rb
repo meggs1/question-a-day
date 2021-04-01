6e94c2b7c3a9bc78f9aba262a2c25620e63f903a
megan <megan.schertz@gmail.com>
2021-03-31 20:51:53 -0500

    add quesitons to seed

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,4 +3,5 @@ class Question < ActiveRecord::Base
     has_many :question_tags
     has_many :users, through: :answers
     has_many :tags, through: :question_tags
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Question.destroy_all
+
+def get_questions
+    site = "https://www.marcandangel.com/2011/03/14/365-thought-provoking-questions-to-ask-yourself-this-year/"
+    doc = Nokogiri::HTML(open(site))
+
+    questions = doc.css("div.entry-content ol li")
+
+    questions.each do |question|
+        name = question.text
+        Question.create(name: name)
+    end
+end
+
+get_questions
