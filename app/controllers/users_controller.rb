26ee72b52f271dd97d302a8da149284164580ac7
megan <megan.schertz@gmail.com>
2021-03-31 21:26:37 -0500

    add user new and show pages and new user form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,25 @@
 class UsersController < ApplicationController
+
+    def new
+        @user= User.new
+    end
+
+    def create # dont render new form redirect
+        user = User.create(user_params)
+        if user.save
+            redirect_to user_path(user)
+        else
+            redirect_to new_user_path
+        end
+    end
+
+    def show # show all of user's answers
+    end
+
+    private
+    
+    def user_params
+        params.require(:user).permit(:name, :username, :password)
+    end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+    has_secure_password
     has_many :answers
     has_many :comments, through: :answers
 end
