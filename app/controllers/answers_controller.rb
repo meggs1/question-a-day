a4fbfd944619580ae040f3334e6a9e643fc58029
megan <megan.schertz@gmail.com>
2021-04-01 15:14:09 -0500

    set up answer show page

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,10 +1,30 @@
 class AnswersController < ApplicationController
+
+    def new
+        if params[:question_id] && !question.exists?(params[:question_id])
+            redirect_to questions_path
+          else
+            @answer = Answer.new(answer_params)
+        end
+    end
     
     def create
-        answer = Answer.new(answer_params)
+       @answer = Answer.new(answer_params)
+        if @answer.save
+            redirect_to answer_path(@answer)
+        else
+            render :new
+        end
     end
 
     def index
+        if current_user
+            @answers = current_user.answers
+        end
+    end
+
+    def show
+        @answer = Answer.find_by(params[:id])
     end
 
     private
