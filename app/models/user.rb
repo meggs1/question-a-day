d24d9ac42b5c43eb85109e6bc1220b4b5357f6b8
megan <megan.schertz@gmail.com>
2021-03-31 21:28:55 -0500

    add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ActiveRecord::Base
     has_secure_password
     has_many :answers
     has_many :comments, through: :answers
+
+    validates :name, presence: true
+    validates :username, presence: true, uniqueness: true
+    validates :password, presence: true
 end
