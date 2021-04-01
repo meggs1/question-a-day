1dfc8d92487c27642204b203d805634415dfc095
megan <megan.schertz@gmail.com>
2021-04-01 01:29:00 -0500

    add current_user helped method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,9 @@
 class ApplicationController < ActionController::Base
-    def current_user
-        if session[:user_id].present?
-          user = User.find_by(:id => session[:user_id])
-        end
-      end
+  helper_method :current_user
+  
+  def current_user
+    if session[:user_id].present?
+      user = User.find_by(:id => session[:user_id])
+    end
+  end
 end
--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -6,7 +6,7 @@ class QuestionsController < ApplicationController
 
     def show
         @question = Question.find(params[:id])
-        @answer = @question.answers.build
+        @answer = @question.answers.build(user_id: current_user.id)
     end
 
 end
