b6cca30a6ff3fb1891a878274dbabf149cc43519
megan <megan.schertz@gmail.com>
2021-04-01 14:53:24 -0500

    get user and sessions login/signup and log out routes to work

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,11 @@ class SessionsController < ApplicationController
   
   def create
     user = User.find_by(:username => params[:user][:username])
-    if user && user.authenticate(params[:password])
+    if user && user.authenticate(params[:user][:password])
       session[:user_id] = user.id
       redirect_to user_path(user)
     else
-      redirect_to root_path
+      redirect_to login_path
     end
   end
 
@@ -16,4 +16,5 @@ class SessionsController < ApplicationController
     session[:user_id] = nil
     redirect_to root_path
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,11 @@ Rails.application.routes.draw do
   
   root 'welcome#index'
 
+  get '/signup' => 'users#new'
+  post '/signup' => 'users#create'
+
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
-  
-  delete '/logout' => 'sessions#destroy'
+
+  get '/logout' => 'sessions#destroy'
 end
