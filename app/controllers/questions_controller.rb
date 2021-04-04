1d29b3c14000caf0eee77f18fb8f583e3abebcdc
megan <megan.schertz@gmail.com>
2021-04-04 17:59:52 -0500

    add new question page and form

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,19 @@
 class QuestionsController < ApplicationController
 
+    def new
+        @question = Question.new
+        @tags = Tag.all
+    end
+
+    def create
+        question = Question.create(question_params)
+        if question.save
+            redirect_to question_path(question)
+        else
+            render :new
+        end
+    end
+
     def index
         @questions = Question.all
     end
@@ -7,6 +21,12 @@ class QuestionsController < ApplicationController
     def show
         @question = Question.find(params[:id])
         @answer = @question.answers.build(user_id: current_user.id)
+    end
+
+    private
+
+    def question_params
+        params.require(:question).permit(:name, tag_ids:[])
     end
 
 end
