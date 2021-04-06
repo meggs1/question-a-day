359719ccda5a9ea7de0dbb92fb88aec0d75c361a
megan <megan.schertz@gmail.com>
2021-04-05 19:00:51 -0500

    add is_answered method to question

--- app/models/question.rb
+++ app/models/question.rb
@@ -7,5 +7,11 @@ class Question < ActiveRecord::Base
 
     #validates new_question (user can only answer a question they haven't answered yet)
     #validates who, what, where, when or why
-
+    def is_answered?
+        if self.answers.blank? || self.answers.nil?
+            false
+        else
+            true
+        end
+    end
 end
