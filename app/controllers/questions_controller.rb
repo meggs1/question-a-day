d6302ab2227fd3d58fa0eb894ea02b7dd02b85bc
megan <megan.schertz@gmail.com>
2021-04-05 23:35:03 -0500

    add conditional to answer form on question show page

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -20,7 +20,9 @@ class QuestionsController < ApplicationController
 
     def show
         @question = Question.find(params[:id])
-        @answer = @question.answers.find_or_create_by(user_id: current_user.id)
+        @user_answer = @question.answers.find_by(user_id: current_user.id)
+        @answer = @question.answers.build(user_id: current_user.id)
+        # @answer = @question.answers.find_or_create_by(user_id: current_user.id)
     end
 
     def edit
