2f914c5d276c75a3e8f3ef8c9d2bdd03e8483f40
megan <megan.schertz@gmail.com>
2021-04-06 21:56:18 -0500

    add tag edit and delete pages and routes

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,5 +1,4 @@
 class TagsController < ApplicationController
-
     def new
         @tag = Tag.new
     end
@@ -7,7 +6,6 @@ class TagsController < ApplicationController
     def create
         tag = Tag.create(tag_params)
         if tag.save
-            session[:tag_id] = tag.id
             redirect_to tag_path(tag)
         else
             redirect_to new_tag_path
@@ -21,6 +19,29 @@ class TagsController < ApplicationController
     def show
         @tag = Tag.find_by_id(params[:id])
         @questions = @tag.questions
+    end
+
+    def edit
+        @tag = Tag.find_by_id(params[:id])
+    end
+
+    def update
+        tag = Tag.update(tag_params)
+        if tag
+            redirect_to tag_path(tag)
+        else
+            redirect_to new_tag_path
+        end
+    end
+
+    def destroy
+        @tag = Tag.find(params[:id])
+        if @tag
+          @tag.destroy
+          redirect_to tags_path
+        else
+          redirect_to tag_path(@tag)
+        end
     end
 
     private
