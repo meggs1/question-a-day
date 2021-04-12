90495d537821c53cea140a5684a4ca1e564b2d5c
megan <megan.schertz@gmail.com>
2021-04-12 16:05:02 -0500

    made todays_question a helper method

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,5 @@
 class WelcomeController < ApplicationController
   def index
-    @todays_question = Question.todays_question
     if current_user
       redirect_to user_path(current_user)
     end
--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,3 +1,7 @@
 module QuestionsHelper
-    
+    def todays_question #add extra day for leap year to seed
+        questions = Question.all
+        date = Date.today.yday
+        questions[(date - 1) % questions.size]
+    end
 end
