6fb89c99fae7f86866ee40cfd82bd5d4f456791c
megan <megan.schertz@gmail.com>
2021-04-13 01:32:24 -0500

    refactor questions controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,37 +1,19 @@
 class QuestionsController < ApplicationController
-
-    # def new
-    #     @question = Question.new
-    #     @tags = Tag.all
-    # end
-
-    # def create
-    #     question = Question.create(question_params)
-    #     if question.save
-    #         redirect_to question_path(question)
-    #     else
-    #         render :new
-    #     end
-    # end
-
+    before_action :find_question only: [:show, :edit, :update]
     def index
         @questions = Question.all
     end
 
     def show
-        @question = Question.find(params[:id])
         @user_answer = @question.answers.find_by(user_id: current_user.id)
-        # @answer = @question.answers.build(user_id: current_user.id)
     end
 
     def edit
-        @question = Question.find(params[:id])
     end
 
     def update
-        question = Question.find(params[:id])
-        if question.update(question_params)
-            redirect_to question_path(question)
+        if @question.update(question_params)
+            redirect_to question_path(@question)
         else
             render :edit
         end
@@ -47,4 +29,7 @@ class QuestionsController < ApplicationController
         params.require(:question).permit(:name, tag_ids:[])
     end
 
+    def find_question
+        @question = Question.find(params[:id])
+    end
 end
