dd3d1f32077af330959d0a901cf793cfbaabf16a
megan <megan.schertz@gmail.com>
2021-04-23 20:02:56 -0500

    remove name validation, not needed

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,8 +3,6 @@ class Question < ActiveRecord::Base
     has_many :question_tags
     has_many :users, through: :answers
     has_many :tags, through: :question_tags
-    
-    validates :name, presence: true, uniqueness: true
 
     def self.most_answered
         all.joins(:answers).group("question_id").order("count(question_id) desc").limit(5)
