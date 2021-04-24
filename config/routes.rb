e25859799b85a7f8a550db366c9101e3acf5d7e1
megan <megan.schertz@gmail.com>
2021-04-23 19:56:32 -0500

    update question edit page, remove form partial and update route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :users, only: [:new, :create, :show]
-  resources :questions, only: [:show, :index, :edit] do 
+  resources :questions, only: [:show, :index, :edit, :update] do 
     resources :answers, only: [:new, :edit, :index, :show]
   end
   resources :answers
