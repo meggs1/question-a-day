f6d505aa564ebdb793313e7e9fc18513504da8fa
megan <megan.schertz@gmail.com>
2021-04-25 19:25:49 -0500

    changed self.most_answered in to a scope method

--- app/models/question.rb
+++ app/models/question.rb
@@ -4,8 +4,5 @@ class Question < ActiveRecord::Base
     has_many :users, through: :answers
     has_many :tags, through: :question_tags
 
-    def self.most_answered
-        all.joins(:answers).group("question_id").order("count(question_id) desc").limit(5)
-    end
-
+    scope :most_answered, -> {self.joins(:answers).group("question_id").order("count(question_id) desc").limit(5)}
 end
