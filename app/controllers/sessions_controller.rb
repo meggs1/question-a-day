8b8a82cfc8ca8ae084bd2775333c204862915f28
megan <megan.schertz@gmail.com>
2021-04-27 16:53:02 -0500

    add warning to session login and changed most_answered scope to use user_id as count

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,6 +9,7 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to root_path
     else
+      flash[:warning] = "Invalid login credentials"
       redirect_to '/login'
     end
   end
