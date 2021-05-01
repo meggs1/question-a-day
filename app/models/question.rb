d4ce153fdcac29a240dd3662cc5efead35dc6bdc
megan <megan.schertz@gmail.com>
2021-05-01 02:45:54 -0500

    testing scope

--- app/models/question.rb
+++ app/models/question.rb
@@ -4,7 +4,8 @@ class Question < ActiveRecord::Base
     has_many :users, through: :answers
     has_many :tags, through: :question_tags
 
-    scope :most_answered, -> {self.joins(:answers).group("question_id").order("count(user_id) desc").limit(5)}
+    # scope :most_answered, -> {self.joins(:answers).group("question_id").order("count(user_id) desc").limit(5)}
+    scope :most_answered, -> {    self.joins(:answers).select("question_id").group("answers.question_id").order("COUNT(user_id) DESC").limit(5)}
 
     def self.todays_question
         questions = Question.all
